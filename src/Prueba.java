d0c342fb633257f55f4686d0eb878948ceed624d
Juliosantos2105 <juliosanas@gmail.com>
2018-03-06 09:01:36 +0100

    commit 1 subida proyecto 6 marzo

---
+++ src/Prueba.java
@@ -0,0 +1,11 @@
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("eh, vegetta. eh, vegetta. eres el mejor youtuber");
+
+	}
+
+}
